52c55748ae82621e4c69d36ebbdff60403d85f23
Dragaar <rosivanyshyn@gmail.com>
2022-09-26 19:27:33 +0300

    impl conversion Path in File Tree

--- src/main/java/com/efimchick/ifmo/io/filetree/FileTreeImpl.java
+++ src/main/java/com/efimchick/ifmo/io/filetree/FileTreeImpl.java
@@ -1,5 +1,149 @@
 package com.efimchick.ifmo.io.filetree;
 
-public class FileTreeImpl implements FileTree {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
+public class FileTreeImpl implements FileTree {
+    @Override
+    public Optional<String> tree(Path path) {
+
+        if(path == null ||  !Files.exists(path)) return Optional.empty();
+        StringBuilder builder = new StringBuilder();
+
+        if(Files.isDirectory(path)){ // якщо це директорія
+            walkByPath(path, builder);
+           // System.out.println(builder.toString());
+
+        } else { // якщо це файл
+            walkByPath(path, builder);
+            System.out.println(builder.toString());
+        }
+
+
+            try(BufferedWriter bfwriter = new BufferedWriter(new FileWriter("E:\\Work\\Dev\\EPAM\\Secnd\\Project\\Streams-IO\\TestFiles\\res.txt")))
+            {
+                bfwriter.write(builder.toString());
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        return Optional.of(builder.toString());
+    }
+    private void walkByPath2(Path path, StringBuilder builder){
+        int maxDepth = 5;
+        int coursorSubDirectories = 0;
+        try (Stream<Path> pathStream = Files.walk(path, maxDepth)) {
+            pathStream.forEach(path1 -> {
+                try {
+                    if(Files.isDirectory(path1)){
+                        builder.append(path1.getFileName() + " " + Files.size(path1) + " bytes \n");
+                    }
+                    else {
+                        if(path1.getRoot() == null) builder.append("└─"); else builder.append("├─");
+                        builder.append(path1.getFileName() + " " + Files.size(path1) + " bytes \n");
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void walkByPath(Path path, StringBuilder builder) {
+        //https://habr.com/ru/post/437694/
+        try {
+            Files.walkFileTree(path, new FileVisitor<Path>() {
+                int subDirectoriesIndex = 0; //рівень глубини наявної директорії
+
+                boolean isElementLast = false;
+               ArrayList<Boolean> isDirectoryLast = new ArrayList<>();
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+
+                    isDirectoryLast.add(false);
+                    //if(dir.equals(getLastFileInParentDirectory(dir))) isElementLast = true;
+                    //addPathTreeToBuilder(builder, dir, subDirectoriesIndex, isElementLast);
+
+                    if(dir.equals(getLastFileInParentDirectory(dir))){
+                        isDirectoryLast.set(subDirectoriesIndex, true);
+                        isElementLast = true;
+                    }
+                    addPathTreeToBuilder(builder, dir, subDirectoriesIndex, isDirectoryLast, isElementLast);
+
+                    subDirectoriesIndex++;
+
+                    isElementLast = false;
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+
+                    //System.out.println("getLastFileInParentDirectory "+ getLastFileInParentDirectory(file));
+                    if(file.equals(getLastFileInParentDirectory(file))) isElementLast = true;
+                    addPathTreeToBuilder(builder, file, subDirectoriesIndex, isDirectoryLast, isElementLast);
+
+
+                    isElementLast = false;
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    isDirectoryLast.set(subDirectoriesIndex-1, false);
+                    subDirectoriesIndex--;
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int countElementsInDirectory(Path path){
+        try(Stream<Path> pathStream = Files.walk(path, 1)){
+            return (int)pathStream.count()-1;
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    private Path getLastFileInParentDirectory(Path path){
+        try(Stream<Path> pathStream = Files.walk(path.getParent(), 1)){
+            long count = countElementsInDirectory(path.getParent());
+            return pathStream.skip(count).findFirst().get();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void addPathTreeToBuilder(StringBuilder builder, Path path, int subDirectoriesIndex, ArrayList<Boolean> isDirectoryLast, boolean isElementLast) throws IOException{
+        if(subDirectoriesIndex>0)  {
+            for(int i = 1; i < subDirectoriesIndex; i++) {
+                if(isDirectoryLast.get(i)) builder.append("   ");
+                else builder.append("│  ");
+            }
+            if(!isElementLast) builder.append("├─  ");
+            else builder.append("└─  ");
+        }
+        builder.append(path.getFileName() + " " + Files.size(path) + " bytes \n");
+    }
 }
